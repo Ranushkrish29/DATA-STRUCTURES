5b9227edda11959070766f30ec77a933f9975563
Ranush Krishna <ranush.krishna@gmail.com>
2021-05-20 21:17:36 +0530

    added --> Singly Linked List

--- js/singly_linked_list.js
+++ js/singly_linked_list.js
@@ -28,30 +28,30 @@ head is pointing to the first element and tail is the last element in the list
    
 */
 //create a class node to store the current value and pointer to the next node
-class Node{
-    constructor(val){
-        this.val=val;
-        this.next=null;
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
     }
 }
 //create a SinglyLinkedlist class which uses the node class to do the operations 
-class SinglyLinkedlist{
+class SinglyLinkedlist {
     //at first the head ,tail ,length is set to zero // constructor get executed automaticaly when the class is inti 
-    constructor(){
-        this.head=null;
-        this.tail=null;
-        this.length =0;
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
     }
     //push method -add a new value to end 
-    push(val){
+    push(val) {
         //create a new node by node class and store it
-        let newnode= new Node(val)
+        let newnode = new Node(val)
         //if there is node { EMPTY-list}
-        if(!this.head){
+        if (!this.head) {
             //point the head to the newly created node
-            this.head=newnode;
+            this.head = newnode;
             //and point the tail to same node
-            this.tail=this.head;
+            this.tail = this.head;
             /* like this
                 []    ----> if list have no element then
                 add a new node to the list [23] 
@@ -59,11 +59,11 @@ class SinglyLinkedlist{
              */
         }
         //if there is a node { list have elements }
-        else{
+        else {
             //set the tail.next points the newly created node 
-            this.tail.next=newnode;
+            this.tail.next = newnode;
             // and set the tail to the new node
-            this.tail=newnode;
+            this.tail = newnode;
             /* like this
                 23  34  ----> if list have  element then
                 head=23 and tail=34 // both are points to the same node
@@ -75,39 +75,39 @@ class SinglyLinkedlist{
         return this;
     }
     //traversal method - from start to end
-    traversal(){
+    traversal() {
         //get the head node {which is the first node} and set it as current node
-        let currentnode=this.head;
+        let currentnode = this.head;
         //loop untill the current node is true if there is no current node exit the loop
-        while(currentnode){
+        while (currentnode) {
             //print the value of the current node 
             console.log(currentnode.val);
             //update the current node to the currentnodes next-node pointer [which is first nodes pointer to second node]
-            currentnode=currentnode.next;
+            currentnode = currentnode.next;
         }
         //return the list
         return this
     }
     //pop method - remove the last element from the list [which is tail node]
-    pop(){
+    pop() {
         //store the first element in the list and previouselement as the first element 
-        let currentnode=this.head;
-        let previousnode =currentnode;
+        let currentnode = this.head;
+        let previousnode = currentnode;
         //if there is no element {node} in the list then return undiefine 
-        if(!currentnode)return undefined;
+        if (!currentnode) return undefined;
         //loop untill the  node which have no node after then{tail node}
-        while(currentnode.next){
+        while (currentnode.next) {
             //set prev node  to currentnode and update the currentnode by currentnode.next
-            previousnode=currentnode;
-            currentnode=currentnode.next;
+            previousnode = currentnode;
+            currentnode = currentnode.next;
         }
         //set the tail to the previousnode [which is last eleemnt -1] and previousnode.node =null 
-        this.tail=previousnode;
-        previousnode.next=null;
+        this.tail = previousnode;
+        previousnode.next = null;
         //if list have only one element then head and tail = null
-        if(this.length===1){
-            this.head=null;
-            this.tail=null;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
         }
         //decreament length
         this.length--;
@@ -115,139 +115,140 @@ class SinglyLinkedlist{
     }
 
     //shifting method -- delete the first element in  the list  {delete head node and repoint to the next node}
-    shifting(){
+    shifting() {
         //store the current head
         let currenthead = this.head;
         //if no element in list return undefined
-        if(!currenthead)return undefined;
+        if (!currenthead) return undefined;
         //if list have only one element then head and tail = null
-        if(this.length===1){
-            this.head=null;
-            this.tail=null;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
         }
         //else -> changer the this.head to currenthead.next {which is next node of the current node} and decreament the length
-        this.head=currenthead.next;
+        this.head = currenthead.next;
         this.length--;
         //return the first heads value
         return currenthead.val;
     }
 
     //unshifting method -- add a new element in the beginning of the list {create new node and point it to the current head and make the new node as the head  }
-    unshifting(val){
+    unshifting(val) {
         //create  a new node with the value
         let newhead = new Node(val);
         //if list have no element then point the head and tail to the newhead
-        if(this.length===0){
-            this.head=newhead;
-            this.tail=this.head;
-        }else{
-        // else point the newhead.next to the current head 
-        newhead.next= this.head;
-        //then set the newhead to the current head and increament the length 
-        this.head=newhead;}
+        if (this.length === 0) {
+            this.head = newhead;
+            this.tail = this.head;
+        } else {
+            // else point the newhead.next to the current head 
+            newhead.next = this.head;
+            //then set the newhead to the current head and increament the length 
+            this.head = newhead;
+        }
         this.length++;
         //return the list
         return this;
     }
     //get method --- return the value in the given index
-    get(index){
+    get(index) {
         //if index is greater then lenght or lesser then 0 return undefined
-        if(index>this.length || index<0)return null;
+        if (index > this.length || index < 0) return null;
         //store the current node , use the counter i
-        let i =0 ,currentnode = this.head;
+        let i = 0, currentnode = this.head;
         //loop if u have curretnode 
-        while(currentnode){
+        while (currentnode) {
             //return the value of currentnode if i === index 
-            if(i===index)return currentnode;
+            if (i === index) return currentnode;
             //update the currentnode to the currentnode.next and increament the i
-            currentnode=currentnode.next; i++;
+            currentnode = currentnode.next; i++;
         }
     }
     //set method --- change the value of the element in the specified index
-    set(index,val){
+    set(index, val) {
         //get function return the specified indexed node
         let foundnode = this.get(index);
         //if the node is present then udapte the node.val to given value and return true
-        if(foundnode ){foundnode.val=val ;return true;}
+        if (foundnode) { foundnode.val = val; return true; }
         else return false;
     }
 
     //insert method ---insert new node in the specified index
-    insert(index,val){
+    insert(index, val) {
         //create a new node with the given value
         let newnode = new Node(val);
         //if index > length or smaller then zero then return false
-        if(index>this.length||index<0){
+        if (index > this.length || index < 0) {
             return false;
         }
         //if index is equal to 0 then add a new element in front {use unshifting method}
-        if(index===0){
+        if (index === 0) {
             this.unshifting(val);
             return true
-        }else 
-        //if index is equal to length then add a new element in last {use push method }
-        if(this.length===index){
-            this.push(val);
-            return true
-        }
+        } else
+            //if index is equal to length then add a new element in last {use push method }
+            if (this.length === index) {
+                this.push(val);
+                return true
+            }
         //get method return the specified indexed node 
         //store the index -1 node in previous
-        let previousnode = this.get(index-1);
+        let previousnode = this.get(index - 1);
         //store index node in next node
-        let nextnode =previousnode.next;
+        let nextnode = previousnode.next;
         //point the previous node --> to the new node and point new node ----> to the next node
-        previousnode.next= newnode; 
-        newnode.next= nextnode; 
+        previousnode.next = newnode;
+        newnode.next = nextnode;
         //increament the length
         this.length++;
-        return true; 
+        return true;
     }
     //remove method -- removes the element in specified list
-    remove(index){
+    remove(index) {
         //if index > length or smaller then zero then return false
-        if(index>=this.length||index<0){
+        if (index >= this.length || index < 0) {
             return undefined;
         }
         //if index is equal to 0 then add a new element in front {use unshifting method}
-        if(index===0){
+        if (index === 0) {
             return this.shifting();
-            
-        }else 
-        //if index is equal to length then add a new element in last {use push method }
-        if(this.length===index-1){
-            return this.pop();
-            
-        }
+
+        } else
+            //if index is equal to length then add a new element in last {use push method }
+            if (this.length === index - 1) {
+                return this.pop();
+
+            }
         //store the index-1 node as pervious and index-node[to be removed node] as a deletenode
-        let previousnode = this.get(index-1);
+        let previousnode = this.get(index - 1);
         let deletenode = previousnode.next;
         //point the previous node ------> deletenodes.next 
         /*  [ 12  ----> 34 -----> 45]  set the pointer of 12 ---> to point the 45  // instead of 34 
             [ 12  -----> 45]   
         */
-        previousnode.next=deletenode.next;
+        previousnode.next = deletenode.next;
         //decreament the list return the value
         this.length--;
         return deletenode.val;
 
     }
     //reverse --reverse the list 
-    reverse(){
+    reverse() {
         let currentnode = this.head;
-        let prevnode=null;
+        let prevnode = null;
         let nextnode;
         //swap the head and tail
-        [this.head ,this.tail]=[this.tail ,this.head]
+        [this.head, this.tail] = [this.tail, this.head]
         //loop the process, length of list times 
-        for(let i=0;i<this.length;i++){
+        for (let i = 0; i < this.length; i++) {
             //set next-node to current-node's next 
             nextnode = currentnode.next;
             //set current-node's next  to previous node 
-            currentnode.next=prevnode;
+            currentnode.next = prevnode;
             //set previous-node to current-node
-            prevnode=currentnode;
+            prevnode = currentnode;
             // set current node to nextnode
-            currentnode=nextnode;
+            currentnode = nextnode;
         }
         //return list
         return this
@@ -269,8 +270,8 @@ list.pop();//delete the last element {tail node}
 list.shifting();//deleter the first element {head node}
 list.unshifting(0);//add new value to the beginning {new head node}
 list.get(4);//get the value in nth[4th] index
-list.set(2,31)//change the value of nth[4th] element to given value {31} 
-list.insert(5,55);//insert new element in the nth[5th] place with the given value {55}
+list.set(2, 31)//change the value of nth[4th] element to given value {31} 
+list.insert(5, 55);//insert new element in the nth[5th] place with the given value {55}
 list.remove(1);//remove the nth[1th] element from the list
 list.reverse();//reverse the list
 
