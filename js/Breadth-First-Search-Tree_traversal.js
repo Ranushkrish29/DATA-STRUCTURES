cbf7fb5aab2fa7992dcd1ed69cca1e8e93afe3dc
Ranush Krishna <ranush.krishna@gmail.com>
2021-05-26 13:29:19 +0530

    add DFPO and BFS tree traversal

---
+++ js/Breadth-First-Search-Tree_traversal.js
@@ -0,0 +1,112 @@
+//----------------------------------BREADTH FIRST SEARCH TREE TRAVERSAL---------------------------------------
+
+/*
+                                                
+                        -->     15              
+                              /    \            
+                    --->   6    --->  25         
+                         /   \       /   \       
+                   ---> 1  ->  8 -> 17  -> 35     
+
+Breadth first search tree traversal will traver all the siblings node of the parent 
+from the above tree the BFS  works like 
+
+    ===>      15             [15]
+              /\
+    ===>    6    25          [15 , 6 , 25]
+           /\    /\
+    ===>  1  8 17  35        [15 , 6 , 25 , 1 , 8 , 17 , 35]
+
+ */
+
+/// CODE TO CREATE A  NEW TREE/////////////////////////////////////////////////// 
+/// -----------------------------------------------------jump to line 66 :)---Breadth-First-Search
+
+class Node{
+    constructor(val){
+        this.value=val;
+        this.left=null;
+        this.right=null;
+        this.duplicate=0;
+    }
+}
+class Binary_search_tree{
+    constructor(){
+        this.root=null;
+    }
+    add(val){
+        let newnode =new Node(val);
+        if(!this.root){ this.root = newnode;return this}
+        let currentnode = this.root;
+        while(currentnode){
+            if(val<currentnode.value){
+                if(!currentnode.left){currentnode.left=newnode; return this}
+                    currentnode=currentnode.left;
+            }else 
+            if(val>currentnode.value){
+                if(!currentnode.right){currentnode.right=newnode; return this}
+                currentnode=currentnode.right;    
+            }else 
+            if(currentnode.value===val){ currentnode.duplicate++; return this}
+        }
+    }
+    find(val){
+        if(!this.root)return undefined
+        let currentnode =this.root, 
+           isfound =false;
+        while(currentnode && !isfound){
+            if (currentnode.value ===val) isfound =true;
+            if(val<currentnode.value)currentnode=currentnode.left; 
+            else if(val>currentnode.value)currentnode=currentnode.right;
+        }
+        return currentnode;
+    }
+   
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////BREADTH FIRST SEARCH STARTS HERE///////////////////////////
+    breadth_first_search(){
+        //to store the current node
+        var node ,
+        //for the tobevisit opeartion FIFO
+        tobevisit =[],
+        //output data
+        visited=[];
+        //push the root to the tobevisit list
+        tobevisit.push(this.root);
+        //true untill length of the tobevisit list is greater then 0
+        while(tobevisit.length){
+            //store the first element from the tobevisit as a node
+            node = tobevisit.shift();
+            //push the node to the visited list
+            visited.push(node.value);
+            //if it have the left-node then add the left-node in the end of the tobevisit list
+            if(node.left)tobevisit.push(node.left);
+            //if it have the right-node then add the right-node in the end of the tobevisit list
+            if(node.right)tobevisit.push(node.right);
+        }
+        //return the visited list\
+        return visited;
+    }
+
+}
+
+let BST = new Binary_search_tree ;
+
+BST.add(15); //adds new element/value to the tree
+BST.add(6);
+BST.add(25);
+BST.add(1);
+BST.add(17);
+BST.add(8);
+BST.add(30);
+BST.add(15);
+
+/* tree looks like this after the values entered                                              
+                        -    15              
+                           /    \            
+                    --   6       25         
+                       /   \     /   \       
+                      1     8   17     35     
+*/
+
+BST.breadth_first_search() // ------ output ------- [15 , 6 , 25 , 1 , 8 , 17 , 35]
---
+++ js/Depth_First_PerOrder_Tree_Traversal.js
@@ -0,0 +1,104 @@
+//---------------------------------- DEPTH FIRST PREORDER TREE TRAVERSAL---------------------------------------
+
+/*
+                                                
+                |           15              
+                V         /    \            
+                |       6       25         
+                V     /  \     /   \       
+                |   1     8   17    35     
+                V   
+Depth first PreOrder tree traversal will traver all the left-side-nodes of the current parent and then right-side-nodes of the parent 
+from the above tree the DFPO  works like 
+
+          15             [15] -----for 15 , 6 is left and 25 is right --- first traver the left side fully , and then traver the right side 25 fully.
+          /\
+        6    25          [15 , 6 , 1 , 8]    
+       /\    /\
+      1  8 17  35        [15 , 6 , 1 , 8 , 35 , 17 , 35]
+
+ */
+
+/// CODE TO CREATE A  TREE/////////////////////////////////////////////////// 
+/// -----------------------------------------------------jump to line 66 :)---Depth-First-PreOrder
+
+class Node{
+    constructor(val){
+        this.value=val;
+        this.left=null;
+        this.right=null;
+        this.duplicate=0;
+    }
+}
+class Binary_search_tree{
+    constructor(){
+        this.root=null;
+    }
+    add(val){
+        let newnode =new Node(val);
+        if(!this.root){ this.root = newnode;return this}
+        let currentnode = this.root;
+        while(currentnode){
+            if(val<currentnode.value){
+                if(!currentnode.left){currentnode.left=newnode; return this}
+                    currentnode=currentnode.left;
+            }else 
+            if(val>currentnode.value){
+                if(!currentnode.right){currentnode.right=newnode; return this}
+                currentnode=currentnode.right;    
+            }else 
+            if(currentnode.value===val){ currentnode.duplicate++; return this}
+        }
+    }
+    find(val){
+        if(!this.root)return undefined
+        let currentnode =this.root, 
+           isfound =false;
+        while(currentnode && !isfound){
+            if (currentnode.value ===val) isfound =true;
+            if(val<currentnode.value)currentnode=currentnode.left; 
+            else if(val>currentnode.value)currentnode=currentnode.right;
+        }
+        return currentnode;
+    }
+   
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////DEPTH FIRST PREORDER STARTS HERE///////////////////////////
+    depth_first_preorder(){
+        //create a empty list to store the elements visited
+        var visited=[];
+        //helper function for the recursion --- accepts the node 
+        function recursion_helper(node){
+            //add the current node's value in end of the visited list
+            visited.push(node.value);
+            //if current-node have the left-node then recurse the node.left to the same helper funtion
+            if(node.left)recursion_helper(node.left);
+            //if current-node have the right-node then recurse the node.right to the same helper funtion
+            if(node.right)recursion_helper(node.right);
+        }
+        //call the helper function with arg of the current root 
+        recursion_helper(this.root);
+        return visited;
+    }
+}
+
+let BST = new Binary_search_tree ;
+
+BST.add(15); //adds new element/value to the tree
+BST.add(6);
+BST.add(25);
+BST.add(1);
+BST.add(17);
+BST.add(8);
+BST.add(35);
+BST.add(15);
+
+/* tree looks like this after the values entered                                              
+                        -    15              
+                           /    \            
+                    --   6       25         
+                       /   \     /   \       
+                      1     8   17     35     
+*/
+
+BST.depth_first_preorder() // ------ output ------- [15 , 6 , 1 , 8 , 25 , 17 , 35]
