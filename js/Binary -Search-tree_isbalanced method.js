ac85ae18757955e835bc22417c32470b165504fa
Ranush Krishna <ranush.krishna@gmail.com>
2021-06-13 15:04:04 +0530

    added BST -isbalanced method

--- js/Binary -Search-tree_isbalanced method.js
+++ js/Binary -Search-tree_isbalanced method.js
@@ -40,35 +40,37 @@ class Binary_search_tree {
         }
     }
     isbalanced() {
-        // rootL to store the height/depth of the root's left child
-        // rootR to store the height/depth of the root's Right child
-        // diff to store the differnce between rootL - rootR
-        let rootL, rootR, diff;
         //if no root  return tree  ----> An empty tree is height-balanced
         if (!this.root) return true;
         //recrusion helper function  -----. accepts an node
         function recursionhelper(node) {
             //base case for the recursion ---> return -1 if the node is null
-            if (node === null) return -1;
+            if (!node) return -1;
             //leftchild and rightchild stores the returned values of their recursive funtion 
             let leftchild = recursionhelper(node.left);
             let rightchild = recursionhelper(node.right);
             //x stores the differnce between  leftchild and rightchild
             let x = leftchild - rightchild;
             //if x = 0 (or) x = -1 (or) x = 1 
-            if (x === 0 || Math.abs(x) === 1) {
+            if (x === 0 || Math.abs(x) === 1)
                 //return the maximim from (leftchild and rightchild) + 1 
                 return Math.max(leftchild, rightchild) + 1;
-            }
         }
-        //to recurse the left child of the root
-        rootL = recursionhelper(this.root.left)
-        //to recurse the right child of the root
-        rootR = recursionhelper(this.root.right)
-        //differnce
-        diff = Math.abs(rootL - rootR);
         //returns true if differnce is equal then 1  or 0  else return false 
-        return (diff === 0 || diff === 1);
+        return Math.abs(recursionhelper(this.root.left) - recursionhelper(this.root.right)) <= 1;
+    }
+
+    //with out the comments...........
+    isbalanced2() {
+        function recursionhelper(node) {
+            if (!node) return -1;
+            let leftchild = recursionhelper(node.left);
+            let rightchild = recursionhelper(node.right);
+            let x = leftchild - rightchild;
+            if (x === 0 || Math.abs(x) === 1)
+                return Math.max(leftchild, rightchild) + 1;
+        }
+        return Math.abs((this.root) ? recursionhelper(this.root.left) - recursionhelper(this.root.right) : 0) <= 1;
     }
 }
 
@@ -95,10 +97,10 @@ BST.add(12);
                  12
 */
 
-console.log(BST.isbalanced());//false
+console.log(BST.isbalanced2());//false
 
 
-//////////////////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////////////
 
 let BST2 = new Binary_search_tree();
 BST2.add(50); //adds new element/value to the tree
@@ -112,20 +114,20 @@ BST2.add(9);
 BST2.add(14);
 BST2.add(19);
 BST2.add(67);
-/* tree looks like this after the values entered                                              
-                             50              
-                           /    \            
-                         17       72         
-                   /----/   \     / \          
-                 12        23  54   76
-                /  \      /      \ 
-               9   14    19       67 
-                 
-*/
+// /* tree looks like this after the values entered                                              
+//                              50              
+//                            /    \            
+//                          17       72         
+//                    /----/   \     / \          
+//                  12        23  54   76
+//                 /  \      /      \ 
+//                9   14    19       67 
+
+// */
 console.log(BST2.isbalanced());//true
 
 
-///////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////
 let BST1 = new Binary_search_tree();
 BST1.add(50);
 BST1.add(40);
@@ -136,16 +138,20 @@ BST1.add(20);
 BST1.add(80);
 BST1.add(10);
 BST1.add(90);
-/* tree looks like this after the values entered
-                             50
-                           /    \
-                         40       60
-                        /           \
-                       30            70
-                      /                \
-                    20                  80     
-                   /                      \
-                 10                       100   
-                
-*/
+// /* tree looks like this after the values entered
+//                              50
+//                            /    \
+//                          40       60
+//                         /           \
+//                        30            70
+//                       /                \
+//                     20                  80     
+//                    /                      \
+//                  10                       100   
+
+// */
 console.log(BST1.isbalanced());//false
+
+let BST4 = new Binary_search_tree()
+
+BST4.isbalanced2();
